1d31b75eca3767adb2135057f5a3437362283f11
andreasoie <oeie.andreas@gmail.com>
2022-03-29 00:17:01 +0200

    added functions for data analysis and wrote about a few initial observations

--- tools/analysis_helpers.py
+++ tools/analysis_helpers.py
@@ -1,6 +1,11 @@
+from __future__ import annotations
+
 import matplotlib.pyplot as plt
+import pandas as pd
 import torch
 import torchvision
+import json
+from typing import Dict, List, Tuple, Union
 
 plt.rcParams["text.usetex"] = True
 
@@ -10,6 +15,69 @@ from omegaconf import OmegaConf
 from omegaconf.dictconfig import DictConfig
 from torch.utils.data.dataloader import _MultiProcessingDataLoaderIter
 from vizer.draw import draw_boxes
+from PIL import ImageFont
+
+
+def get_category_metrics(category_ids: pd.DataFrame, decimals: float = 2) -> None:
+    # Series to DataFrame
+    label_distribution = pd.DataFrame(
+        category_ids.value_counts().sort_values(ascending=False)
+    )
+    # Rename
+    label_distribution.index.name = "category"
+    # Rename
+    label_distribution.rename(columns={"category_id": "total"}, inplace=True)
+    # Cummulative sum
+    total_labels = label_distribution["total"].sum()
+    # Add percentage column for simpler intuitive analysis
+    label_distribution["in %"] = label_distribution["total"] / total_labels
+    # Prettify
+    label_distribution["in %"] = label_distribution["in %"] * 100
+    label_distribution["in %"] = label_distribution["in %"].round(decimals)
+    label_distribution.reset_index(inplace=True)
+    return label_distribution
+
+
+def process_annotations(loaded_annotations: List[dict]) -> pd.DataFrame:
+    cleaned_annotations = []
+    for annotation in loaded_annotations:
+        # Ignore segmentation
+        del annotation["segmentation"]
+        # Rescaled image sizes
+        xmin = annotation["bbox"][0]
+        ymin = annotation["bbox"][1]
+        xmax = annotation["bbox"][2] + xmin
+        ymax = annotation["bbox"][3] + ymin
+        # Create rows with rescaled coordinates for
+        # easier visualization and analysis
+        cleaned_annotations.append(
+            {
+                "id": annotation["id"],
+                "image_id": annotation["image_id"],
+                "category_id": annotation["category_id"],
+                "area": annotation["area"],
+                "xmin": xmin,
+                "ymin": ymin,
+                "xmax": xmax,
+                "ymax": ymax,
+                "width": xmax - xmin,
+                "height": ymax - ymin,
+            }
+        )
+    return pd.DataFrame(cleaned_annotations)
+
+
+def process_categories(loaded_catagories: List[dict]) -> pd.DataFrame:
+    return pd.DataFrame(loaded_catagories).drop(columns=["supercategory"])
+
+
+def load_annotation_file(cfg: OmegaConf) -> Union[Dict, None]:
+    _file = cfg["data_train"]["dataset"]["annotation_file"]
+    if _file.endswith(".json"):
+        with open(_file, "r") as f:
+            return json.load(f)
+    else:
+        raise NotImplementedError("Only json are supported!")
 
 
 def prepare_dataset(cfg: OmegaConf, dataset_type: str, batch_size: int):
@@ -34,7 +102,7 @@ def prepare_dataset(cfg: OmegaConf, dataset_type: str, batch_size: int):
     return cfg, dataset, gpu_transform
 
 
-def yield_and_vizulize_sample(
+def yield_and_visualize_sample(
     it: _MultiProcessingDataLoaderIter,
     lblmap: DictConfig,
     tfms: torchvision.transforms.Compose,
@@ -51,9 +119,10 @@ def yield_and_vizulize_sample(
     Key [image_id]: shape=torch.Size([1]), dtype=torch.int64
     """
 
-    # Preprocess
     sample = next(it)
     sample = tfms(sample)
+
+    # Preprocess
     image = sample["image"] * image_std + image_mean
     image = (image * 255).byte()[0]
     boxes = sample["boxes"][0]
@@ -62,12 +131,14 @@ def yield_and_vizulize_sample(
 
     # Add boxes
     im = image.permute(1, 2, 0).cpu().numpy()
+    custom_font = ImageFont.truetype("Dyuthi-Regular.ttf", size=12)
     im = draw_boxes(
         image=im,
         boxes=boxes.cpu().numpy(),
         labels=sample["labels"][0].cpu().numpy().tolist(),
         class_name_map=lblmap,
         width=2,
+        font=custom_font,
     )
 
     plt.figure(figsize=viz_cfg["figsize"], dpi=viz_cfg["dpi"])
