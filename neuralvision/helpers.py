9b04df24e9537156ac17eb5f4fbad6d2c39ea531
andreasoie <oeie.andreas@gmail.com>
2022-04-06 23:46:28 +0200

    types

--- neuralvision/helpers.py
+++ neuralvision/helpers.py
@@ -1,15 +1,16 @@
 from os import PathLike
 from pathlib import Path
-from typing import Union
+from typing import Dict, List, Union, Any
 
 import numpy as np
 import torch
 from torch.utils.data._utils.collate import default_collate
 
 from neuralvision.tops.config.lazy import LazyConfig
+from omegaconf import DictConfig, ListConfig, OmegaConf
 
 
-def load_config(config_path: PathLike):
+def load_config(config_path: PathLike) -> DictConfig:
     config_path = Path(config_path)
     print(f"Loading config from: {config_path}")
     run_name = "_".join(config_path.parts[1:-1]) + "_" + config_path.stem
@@ -42,13 +43,13 @@ def batch_collate_val(batch):
     return batch_
 
 
-def class_id_to_name(labels, label_map: list):
+def class_id_to_name(labels: Union[torch.Tensor, list], label_map: list) -> List[str]:
     if isinstance(labels, torch.Tensor):
         labels = labels.cpu().numpy().tolist()
     return [label_map[idx] for idx in labels]
 
 
-def tencent_trick(model):
+def tencent_trick(model: torch.nn.Module) -> List[Dict[str, Any]]:
     """
     Divide parameters into 2 groups.
     First group is BNs and all biases.
